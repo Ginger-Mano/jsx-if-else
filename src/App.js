4057c69400c9f5d019fbda1cf969477fcd7870ff
Ginger-Mano <mcasimirian@gmail.com>
2021-01-15 16:18:29 -0500

    starter if-else

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
+import Apples from "./Apples"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    question: false
+  }
+
+  renderApple = () => {
+    if (this.state.question === false) {
+      return "hi"
+    } else {
+      return <Apples />
+    }
+  }
+
+
+
+  render() {
+    // const question = this.state.question;
+    // let ask;
+    // if (question) {
+    //   ask = <Apples />
+    // } else {
+    //   ask = "Hi"
+    // }
+
+    const debate = this.state.question ? "Hi" : <Apples />
+    return (
+
+      <div className="App">
+        <h2>Hello</h2>
+        {this.renderApple()}
+        {debate}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Apples.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class Apples extends Component {
+    render() {
+        return (
+            <div>
+                <h1>How many apples you got?</h1>
+            </div>
+        )
+    }
+}
+export default Apples
